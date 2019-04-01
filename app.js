ad3486cf2d5a2718d01960d3c936b91b9cb7552e
George <admin@georgesimos.com>
2019-04-01 21:07:01 +0300

    Drop a table

--- app.js
+++ app.js
@@ -6,15 +6,22 @@ con.connect(err => {
     console.log('Connected to database')
 })
 
-// // Create Database
+// Create Database
 // con.query('CREATE DATABASE store', (err, res) => {
 //     if (err) throw err;
 //     console.log('Database Created')
 // })
 
 // Create Table
-const sql = 'CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),username VARCHAR(255) UNIQUE, email VARCHAR(255))';
+// const sql = 'CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),username VARCHAR(255) UNIQUE, email VARCHAR(255))';
+// con.query(sql, (err, res) => {
+//     if (err) throw err;
+//     console.log('Table has been created...')
+// })
+
+// Drop Table
+const sql = 'DROP TABLE customers';
 con.query(sql, (err, res) => {
     if (err) throw err;
-    console.log('Table has been created...')
+    console.log('Table has been Droped...')
 })
