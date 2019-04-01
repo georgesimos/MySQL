f7189b5f2be66310f22adb3d40d87bbc6ad032ac
George <admin@georgesimos.com>
2019-04-01 21:08:32 +0300

    Insert Many

--- app.js
+++ app.js
@@ -34,8 +34,21 @@ con.connect(err => {
 // })
 
 // Insert Into
-const sql = 'INSERT INTO customers (name, email, username) VALUES ("Tester Simos", "tester@gmail.com","tester")';
-con.query(sql, (err, res) => {
+// const sql = 'INSERT INTO customers (name, email, username) VALUES ("Tester Simos", "tester@gmail.com","tester")';
+// con.query(sql, (err, res) => {
+//     if (err) throw err;
+//     console.log('Data Inserted Into Table...')
+// })
+
+// Insert Into (Many)
+const sql = 'INSERT INTO customers (name, email, username) VALUES ?';
+const values = [
+    ['Tim', 'tim@tim.com', 'tim'],
+    ['tina', 'tina@tina.com', 'tina'],
+    ['lara', 'lara@lara.com', 'lara'],
+]
+con.query(sql, [values], (err, res) => {
     if (err) throw err;
-    console.log('Data Inserted Into Table...')
+    console.log(res)
+    console.log('Records Inserted Into Table...: ' + res.affectedRows)
 })
